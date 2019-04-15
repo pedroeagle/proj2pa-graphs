b8c8f70349e5b6780b6dfb08c59033584efcd69a
Pedro Igor <pedroigor.silva@outlook.com>
2019-04-15 14:21:30 -0300

    Criados arquivos de interface para a IDE Idea da Jetbrains.

---
+++ src/view/MainPage.java
@@ -0,0 +1,10 @@
+package view;
+
+import javax.swing.*;
+
+public class MainPage {
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+}
